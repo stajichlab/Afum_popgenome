cc3ddabc5a2c4a22761182a6cc6950f6cd2f534d
Jason Stajich <jasonstajich.phd@gmail.com>
2018-11-04 22:26:57 -0800

    switch to bcf files and all-in-one sorting and converting

--- variantcall/Variants_filter/make_tab_files.sh
+++ variantcall/Variants_filter/make_tab_files.sh
@@ -1,11 +1,10 @@
 module load bcftools/1.9
 
 bcftools query -l A_fumigiatus_Af293.Popgen2.selected.SNP.vcf.gz | grep -v COH > COH_exclude.txt
-bcftools view -S COH_exclude.txt A_fumigiatus_Af293.Popgen2.selected.SNP.vcf.gz > A_fumigiatus_Af293.Popgen2.selected.SNP.noCOH.vcf
-bcftools +fill-tags A_fumigiatus_Af293.Popgen2.selected.SNP.noCOH.vcf > A_fumigiatus_Af293.Popgen2.selected.SNP.noCOH.retag.vcf
+bcftools concat -Ou -a A_fumigiatus_Af293.Popgen2.selected.SNP.vcf.gz A_fumigiatus_Af293.Popgen2.selected.INDEL.vcf.gz | bcftools view -Ou -S COH_exclude.txt - | bcftools +fill-tags -Ob | bcftools sort -Ob -o A_fumigiatus_Af293.Popgen2.selected.noCOH.retag.bcf -
 
-for file in *.vcf; do 
-	b=$(basename $file .vcf); 
+for file in *.bcf; do 
+	b=$(basename $file .bcf); 
 	bcftools query -H -f '%CHROM\t%POS\t%INFO/AF\t%INFO/AC\t%INFO/AN\t%REF\t%ALT{0}[\t%TGT]\n' $file > $b.tab 
 done
 
