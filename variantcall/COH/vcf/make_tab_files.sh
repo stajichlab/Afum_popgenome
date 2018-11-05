cc6bdc81daeb03dd85bd4d9521ba543897f0a8e0
Jason Stajich <jasonstajich.phd@gmail.com>
2018-11-04 22:41:51 -0800

    update to merge INDEL and SNP

--- variantcall/COH/vcf/make_tab_files.sh
+++ variantcall/COH/vcf/make_tab_files.sh
@@ -1,7 +1,12 @@
 module load bcftools/1.9
 
-for file in *.selected*.vcf.gz; do 
-	b=$(basename $file .vcf.gz); 
+
+bcftools concat -Ou -a COH.Run1.selected.SNP.vcf.gz COH.Run1.selected.INDEL.vcf.gz | bcftools sort -Ob -o COH.Run1.selected.bcf
+bcftools concat -Ou -a COH.Run1.selected_nofixed.SNP.vcf.gz COH.Run1.selected_nofixed.INDEL.vcf.gz | bcftools sort -Ob -o COH.Run1.selected_nofixed.bcf
+
+for file in *.bcf; do 
+	b=$(basename $file .bcf); 
 	bcftools query -H -f '%CHROM\t%POS\t%INFO/AF\t%INFO/AC\t%INFO/AN\t%REF\t%ALT{0}[\t%TGT]\n' $file > $b.tab 
+	pigz $b.tab
 done
 
