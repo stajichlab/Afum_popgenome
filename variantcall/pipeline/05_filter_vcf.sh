a8b3f9d721856d46314d495678082f616f1b7178
Jason Stajich <jasonstajich.phd@gmail.com>
2019-01-10 22:52:51 -0800

    migrate to one less step, renumber

--- variantcall/pipeline/05_filter_vcf.sh
+++ variantcall/pipeline/05_filter_vcf.sh
@@ -1,98 +1,137 @@
 #!/usr/bin/bash
 #SBATCH --nodes 1 -p short
 #SBATCH --ntasks 1
-#SBATCH --mem-per-cpu 16G
+#SBATCH --mem 16G
 #SBATCH --job-name=GATK.select_filter
-#SBATCH --output=logs/GATK.select_filter.%A.log
+#SBATCH --output=logs/GATK.select_filter.log
 
-module load gatk/3.8
+module load gatk/4.0.8.1
 module unload java
 module load java/8
-INDIR=Variants
-OUTDIR=Variants_filter
-GENOME=genome/Af293.fasta
+hostname
 
-mkdir -p $OUTDIR
-for PREFIX in A_fumigiatus_Af293.Popgen1 A_fumigiatus_Af293.Popgen2
-do
-    echo $PREFIX
-    INFILE=$INDIR/$PREFIX.vcf
-    INSNP=$OUTDIR/$PREFIX.SNP.vcf
-    ININDEL=$OUTDIR/$PREFIX.INDEL.vcf
-    FILTEREDSNP=$OUTDIR/$PREFIX.filtered.SNP.vcf
-    FILTEREDINDEL=$OUTDIR/$PREFIX.filtered.INDEL.vcf
-    SNPONLY=$OUTDIR/$PREFIX.selected.SNP.vcf
-    INDELONLY=$OUTDIR/$PREFIX.selected.INDEL.vcf
+CONFIG=config.txt
+if [ -f $CONFIG ]; then
+    source $CONFIG
+fi
+GENOME=$GENOMEFOLDER/$GENOMEFASTA
+CPU=$SLURM_CPUS_ON_NODE
+if [ -z $CPU ]; then
+    CPU=1
+fi
+
+if [ -z $FINALVCF ]; then
+	echo "Need FINALVCF for output"
+	exit
+fi
+echo "$PREFIX $INFILE"
+INFILE=$FINALVCF/$PREFIX.all.vcf.gz
+INSNP=$FINALVCF/$PREFIX.SNP.vcf
+ININDEL=$FINALVCF/$PREFIX.INDEL.vcf
+FILTEREDSNP=$FINALVCF/$PREFIX.filtered.SNP.vcf
+FILTEREDINDEL=$FINALVCF/$PREFIX.filtered.INDEL.vcf
+SNPONLY=$FINALVCF/$PREFIX.selected.SNP.vcf
+INDELONLY=$FINALVCF/$PREFIX.selected.INDEL.vcf
+
+FILTEREDFIXEDSNP=$FINALVCF/$PREFIX.filteredfixed.SNP.vcf
+FILTEREDFIXEDINDEL=$FINALVCF/$PREFIX.filteredfixed.INDEL.vcf
+SNPNOFIXED=$FINALVCF/$PREFIX.selected_nofixed.SNP.vcf
+INDELNOFIXED=$FINALVCF/$PREFIX.selected_nofixed.INDEL.vcf
+
 
  if [ ! -f $INSNP ]; then
-  java -Xmx3g -jar $GATK \
-  -T SelectVariants \
+  gatk SelectVariants \
   -R $GENOME \
   --variant $INFILE \
-  -o $INSNP \
-  -env \
-  -ef \
-  -restrictAllelesTo BIALLELIC \
-  -selectType SNP
+  -O $INSNP \
+  --restrict-alleles-to BIALLELIC \
+  --select-type-to-include SNP
  fi
-
  if [ ! -f $ININDEL ]; then
-  java -Xmx3g -jar $GATK \
-  -T SelectVariants \
+  gatk SelectVariants \
   -R $GENOME \
   --variant $INFILE \
-  -o $ININDEL \
-  -env \
-  -ef \
-  -selectType INDEL -selectType MIXED -selectType MNP
+  --output $ININDEL \
+  --select-type-to-include INDEL --select-type-to-include MIXED --select-type-to-include MNP
  fi
 
  if [ ! -f $FILTEREDSNP ]; then
-   java -Xmx3g -jar $GATK \
-   -T VariantFiltration -o $FILTEREDSNP \
+   gatk VariantFiltration --output $FILTEREDSNP \
    --variant $INSNP -R $GENOME \
-   --clusterWindowSize 10  -filter "QD<2.0" -filterName QualByDepth \
-   -filter "MQ<40.0" -filterName MapQual \
-   -filter "QUAL<100" -filterName QScore \
-   -filter "MQRankSum < -12.5" -filterName MapQualityRankSum \
-   -filter "SOR > 4.0" -filterName StrandOddsRatio \
-   -filter "FS>60.0" -filterName FisherStrandBias \
-   -filter "ReadPosRankSum<-8.0" -filterName ReadPosRank \
-   --missingValuesInExpressionsShouldEvaluateAsFailing 
-
-#-filter "HaplotypeScore > 13.0" -filterName HaplotypeScore
-#-filter "MQ0>=10 && ((MQ0 / (1.0 * DP)) > 0.1)" -filterName MapQualRatio \
+   --cluster-window-size 10  \
+   --filter-expression "QD < 2.0" --filter-name QualByDepth \
+   --filter-expression "MQ < 40.0" --filter-name MapQual \
+   --filter-expression "QUAL < 100" --filter-name QScore \
+   --filter-expression "MQRankSum < -12.5" --filter-name MapQualityRankSum \
+   --filter-expression "SOR > 3.0" --filter-name StrandOddsRatio \
+   --filter-expression "FS > 60.0" --filter-name FisherStrandBias \
+   --filter-expression "ReadPosRankSum < -8.0" --filter-name ReadPosRank \
+   --missing-values-evaluate-as-failing
  fi
 
+ if [ ! -f $FILTEREDFIXEDSNP ]; then
+   gatk VariantFiltration --output $FILTEREDFIXEDSNP \
+   --variant $INSNP -R $GENOME \
+   --cluster-window-size 10  \
+   --filter-expression "AF > 0.99" --filter-name FixedAllele \
+   --filter-expression "QD < 2.0" --filter-name QualByDepth \
+   --filter-expression "MQ < 40.0" --filter-name MapQual \
+   --filter-expression "QUAL < 100" --filter-name QScore \
+   --filter-expression "MQRankSum < -12.5" --filter-name MapQualityRankSum \
+   --filter-expression "SOR > 3.0" --filter-name StrandOddsRatio \
+   --filter-expression "FS > 60.0" --filter-name FisherStrandBias \
+   --filter-expression "ReadPosRankSum < -8.0" --filter-name ReadPosRank \
+   --missing-values-evaluate-as-failing
+
+ fi
  if [ ! -f $FILTEREDINDEL ]; then
-  java -Xmx3g -jar $GATK \
-  -T VariantFiltration -o $FILTEREDINDEL \
+  gatk VariantFiltration --output $FILTEREDINDEL \
   --variant $ININDEL -R $GENOME \
-  --clusterWindowSize 10  -filter "QD<2.0" -filterName QualByDepth \
-  -filter "MQRankSum < -12.5" -filterName MapQualityRankSum \
-  -filter "SOR > 4.0" -filterName StrandOddsRatio \
-  -filter "FS>200.0" -filterName FisherStrandBias \
-  -filter "InbreedingCoeff<-0.8" -filterName InbreedCoef \
-  -filter "ReadPosRankSum<-20.0" -filterName ReadPosRank 
+  --cluster-window-size 10  -filter "QD < 2.0" --filter-name QualByDepth \
+  -filter "MQRankSum < -12.5" --filter-name MapQualityRankSum \
+  -filter "SOR > 4.0" --filter-name StrandOddsRatio \
+  -filter "FS > 200.0" --filter-name FisherStrandBias \
+  -filter "InbreedingCoeff < -0.8" --filter-name InbreedCoef \
+  -filter "ReadPosRankSum < -20.0" --filter-name ReadPosRank 
+ fi
+
+ if [ ! -f $FILTEREDFIXEDINDEL ]; then
+  gatk VariantFiltration --output $FILTEREDFIXEDINDEL \
+  --variant $ININDEL -R $GENOME \
+   --filter-expression "AF > 0.99" --filter-name FixedAllele \
+  --cluster-window-size 10  -filter "QD < 2.0" --filter-name QualByDepth \
+  -filter "MQRankSum < -12.5" --filter-name MapQualityRankSum \
+  -filter "SOR > 4.0" --filter-name StrandOddsRatio \
+  -filter "FS > 200.0" --filter-name FisherStrandBias \
+  -filter "InbreedingCoeff < -0.8" --filter-name InbreedCoef \
+  -filter "ReadPosRankSum < -20.0" --filter-name ReadPosRank 
  fi
 
  if [ ! -f $SNPONLY ]; then
-  java -Xmx16g -jar $GATK \
-   -R $GENOME \
-   -T SelectVariants \
+   gatk SelectVariants -R $GENOME \
    --variant $FILTEREDSNP \
-   -o $SNPONLY \
-   -env \
-   -ef \
-   --excludeFiltered
+   --output $SNPONLY \
+   --exclude-filtered
  fi
 
  if [ ! -f $INDELONLY ]; then
-  java -Xmx16g -jar $GATK \
-   -R $GENOME \
-   -T SelectVariants \
+   gatk SelectVariants -R $GENOME \
    --variant $FILTEREDINDEL \
-   -o $INDELONLY \
-   --excludeFiltered 
+   --output $INDELONLY \
+   --exclude-filtered 
  fi
-done
+
+ if [ ! -f $SNPNOFIXED ]; then
+     gatk SelectVariants -R $GENOME \
+	 --variant $FILTEREDFIXEDSNP \
+	 --output $SNPNOFIXED \
+	 --exclude-filtered
+ fi
+
+ if [ ! -f $INDELNOFIXED ]; then
+     gatk SelectVariants -R $GENOME \
+	 --variant $FILTEREDFIXEDINDEL \
+	 --output $INDELNOFIXED \
+	 --exclude-filtered
+
+ fi
