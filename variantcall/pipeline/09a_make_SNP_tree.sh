505bed79429c53a23b9d47e38303548e3d292b70
Jason Stajich <jasonstajich.phd@gmail.com>
2019-10-20 23:05:21 -0700

    moved speedyslice and tree build into a single step

--- variantcall/pipeline/09a_make_SNP_tree.sh
+++ variantcall/pipeline/09a_make_SNP_tree.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/bash
 
-#SBATCH --mem=24gb --ntasks 16 --nodes 1
-#SBATCH --time=48:00:00
-#SBATCH -J makeTree --out logs/make_tree.log
+#SBATCH --mem=24gb --ntasks 24 --nodes 1
+#SBATCH --time=2:00:00 -p short
+#SBATCH -J maketree --out logs/make_tree.log
+
 
 CPU=2
 if [ $SLURM_CPUS_ON_NODE ]; then
@@ -10,44 +11,52 @@ if [ $SLURM_CPUS_ON_NODE ]; then
 fi
 
 if [[ -f config.txt ]]; then
-	source config.txt
+    source config.txt
 else
-	echo "Need a config.txt"
-	exit
+    echo "Need a config.txt"
+    exit
 fi
 
 if [[ -z $REFNAME ]]; then
-	REFNAME=REF
+    REFNAME=REF
 fi
+module load parallel
+module load perl/5.20.2
 module load bcftools/1.9
 module load samtools/1.9
 module load IQ-TREE
 module load fasttree
 
-mkdir -p $TREEDIR
+print_fas() {
+    printf ">%s\n%s\n" $1 $(bcftools query -e 'INFO/AF < 0.1' -s $1 -f '[%TGT]' $2)
+}
+
+export -f print_fas
+
 for TYPE in SNP INDEL
 do
     root=$FINALVCF/$PREFIX.selected.$TYPE
     FAS=$TREEDIR/$PREFIX.$TYPE.mfa
+
     if [ -f $root.vcf ]; then
 	bgzip $root.vcf
+	tabix $root.vcf.gz
     fi
     vcf=$root.vcf.gz
-    if [ ! -f $vcf.tbi ]; then
-	    tabix $vcf
-    fi
+    printf ">%s\n%s\n" $REFNAME $(bcftools query -e 'INFO/AF < 0.1' -f '%REF' ${vcf}) > $FAS
+    parallel -j $CPU print_fas ::: $(bcftools query -l ${vcf}) ::: $vcf >> $FAS
+
+    perl -ip -e 'if(/^>/){s/[\(\)#]/_/g; s/_+/_/g } else {s/[\*\.]/-/g }' $FAS
+
     tab=$root.bcftools.tab
+
     if [ ! -f $tab ]; then
 	bcftools query -H -f '%CHROM\t%POS\t%INFO/AF\t%INFO/AC\t%INFO/AN\t%REF\t%ALT{0}[\t%TGT]\n' ${vcf} > $tab 
 	bcftools query -H -e 'INFO/AF < 0.1' -f '%CHROM\t%POS\t%INFO/AF\t%INFO/AC\t%INFO/AN\t%REF\t%ALT{0}[\t%TGT]\n' ${vcf} > $root.highfreq.bcftools.tab 
     fi
-    if [ ! -f $FAS ]; then
-	printf ">%s\n%s\n" $REFNAME $(bcftools query -e 'INFO/AF < 0.1' -f '%REF' ${vcf}) > $FAS
-	perl -i -p -e 'if (/^>/) { s/[\(\)#]/_/g; s/_+/_/g; } else { s/[\*\.]/-/g; }' $FAS
-    fi
-    cat tmp_out/$PREFIX.$TYPE.*.fas_seq >> $FAS
-    if [ ! -f $TREEDIR/$PREFIX.$TYPE.fasttree.tre ]; then
+
+    if [[ $TYPE == "SNP" && ! -f $TREEDIR/$PREFIX.$TYPE.fasttree.tre ]]; then
 	FastTreeMP -gtr -gamma -nt < $FAS > $TREEDIR/$PREFIX.$TYPE.fasttree.tre
     fi
+    
 done
-#iqtree-omp -nt $CPU -s $FAS -m GTR+ASC -b 100
