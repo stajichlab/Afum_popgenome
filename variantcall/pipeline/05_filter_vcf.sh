52124ad59114ab4b0ea751b80f8ca9c7d69b9c14
Jason Stajich <jasonstajich.phd@gmail.com>
2019-10-20 23:07:05 -0700

    updated to compress files to save space

--- variantcall/pipeline/05_filter_vcf.sh
+++ variantcall/pipeline/05_filter_vcf.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
-#SBATCH --nodes 1 -p short
-#SBATCH --ntasks 1
+#SBATCH --nodes 1 -p batch
+#SBATCH --ntasks 4
 #SBATCH --mem 16G
 #SBATCH --job-name=GATK.select_filter
 #SBATCH --output=logs/GATK.select_filter.log
@@ -38,100 +38,123 @@ FILTEREDFIXEDINDEL=$FINALVCF/$PREFIX.filteredfixed.INDEL.vcf
 SNPNOFIXED=$FINALVCF/$PREFIX.selected_nofixed.SNP.vcf
 INDELNOFIXED=$FINALVCF/$PREFIX.selected_nofixed.INDEL.vcf
 
+if [[ ! -f $INSNP.gz || $INFILE -nt $INSNP.gz ]]; then
+    gatk SelectVariants \
+	-R $REFGENOME \
+	--variant $INFILE \
+	-O $INSNP \
+	--restrict-alleles-to BIALLELIC \
+	--select-type-to-include SNP --create-output-variant-index false
+    bgzip $INSNP
+    tabix $INSNP.gz
+    unlink $INSNP.idx
+fi
 
- if [ ! -f $INSNP ]; then
-  gatk SelectVariants \
-  -R $REFGENOME \
-  --variant $INFILE \
-  -O $INSNP \
-  --restrict-alleles-to BIALLELIC \
-  --select-type-to-include SNP
- fi
- if [ ! -f $ININDEL ]; then
-  gatk SelectVariants \
-  -R $REFGENOME \
-  --variant $INFILE \
-  --output $ININDEL \
-  --select-type-to-include INDEL --select-type-to-include MIXED --select-type-to-include MNP
- fi
+if [[ ! -f $ININDEL.gz || $INFILE -nt $ININDEL.gz ]]; then
+    gatk SelectVariants \
+	-R $REFGENOME \
+	--variant $INFILE \
+	--output $ININDEL \
+	--select-type-to-include INDEL --select-type-to-include MIXED \
+	--select-type-to-include MNP --create-output-variant-index false
+    bgzip $ININDEL
+    tabix $ININDEL.gz
+fi
+    
+if [[ ! -f $FILTEREDSNP.gz || $INSNP.gz -nt $FILTEREDSNP.gz ]]; then
+    gatk VariantFiltration --output $FILTEREDSNP \
+	--variant $INSNP.gz -R $REFGENOME \
+	--cluster-window-size 10  \
+	--filter-expression "QD < 2.0" --filter-name QualByDepth \
+	--filter-expression "MQ < 40.0" --filter-name MapQual \
+	--filter-expression "QUAL < 100" --filter-name QScore \
+	--filter-expression "MQRankSum < -12.5" --filter-name MapQualityRankSum \
+	--filter-expression "SOR > 4.0" --filter-name StrandOddsRatio \
+	--filter-expression "FS > 60.0" --filter-name FisherStrandBias \
+	--filter-expression "ReadPosRankSum < -8.0" --filter-name ReadPosRank \
+	--missing-values-evaluate-as-failing --create-output-variant-index false
+    bgzip $FILTEREDSNP
+    tabix $FILTEREDSNP.gz
+fi
 
- if [ ! -f $FILTEREDSNP ]; then
-   gatk VariantFiltration --output $FILTEREDSNP \
-   --variant $INSNP -R $REFGENOME \
-   --cluster-window-size 10  \
-   --filter-expression "QD < 2.0" --filter-name QualByDepth \
-   --filter-expression "MQ < 40.0" --filter-name MapQual \
-   --filter-expression "QUAL < 100" --filter-name QScore \
-   --filter-expression "MQRankSum < -12.5" --filter-name MapQualityRankSum \
-   --filter-expression "SOR > 3.0" --filter-name StrandOddsRatio \
-   --filter-expression "FS > 60.0" --filter-name FisherStrandBias \
-   --filter-expression "ReadPosRankSum < -8.0" --filter-name ReadPosRank \
-   --missing-values-evaluate-as-failing
- fi
+if [[ ! -f $FILTEREDFIXEDSNP.gz || $INSNP.gz -nt $FILTEREDFIXEDSNP.gz ]]; then
+    gatk VariantFiltration --output $FILTEREDFIXEDSNP \
+	--variant $INSNP.gz -R $REFGENOME \
+	--cluster-window-size 10  \
+	--filter-expression "QD < 2.0" --filter-name QualByDepth \
+	--filter-expression "MQ < 40.0" --filter-name MapQual \
+	--filter-expression "QUAL < 100" --filter-name QScore \
+	--filter-expression "MQRankSum < -12.5" --filter-name MapQualityRankSum \
+	--filter-expression "SOR > 4.0" --filter-name StrandOddsRatio \
+	--filter-expression "FS > 60.0" --filter-name FisherStrandBias \
+	--filter-expression "ReadPosRankSum < -8.0" --filter-name ReadPosRank \
+	--filter-expression "AF > 0.99" --filter-name FixedAllele \
+	--missing-values-evaluate-as-failing --create-output-variant-index false
+    bgzip $FILTEREDFIXEDSNP
+    tabix $FILTEREDFIXEDSNP.gz
+fi
 
- if [ ! -f $FILTEREDFIXEDSNP ]; then
-   gatk VariantFiltration --output $FILTEREDFIXEDSNP \
-   --variant $INSNP -R $REFGENOME \
-   --cluster-window-size 10  \
-#   --filter-expression "AF > 0.99" --filter-name FixedAllele \
-   --filter-expression "QD < 2.0" --filter-name QualByDepth \
-   --filter-expression "MQ < 40.0" --filter-name MapQual \
-   --filter-expression "QUAL < 100" --filter-name QScore \
-   --filter-expression "MQRankSum < -12.5" --filter-name MapQualityRankSum \
-   --filter-expression "SOR > 3.0" --filter-name StrandOddsRatio \
-   --filter-expression "FS > 60.0" --filter-name FisherStrandBias \
-   --filter-expression "ReadPosRankSum < -8.0" --filter-name ReadPosRank \
-   --missing-values-evaluate-as-failing
+if [[ ! -f $FILTEREDINDEL.gz || $FILTEREDINDEL.gz -nt $ININDEL.gz ]]; then
+    gatk VariantFiltration --output $FILTEREDINDEL \
+	--variant $ININDEL.gz -R $REFGENOME \
+	--cluster-window-size 10  -filter "QD < 2.0" --filter-name QualByDepth \
+	-filter "MQRankSum < -12.5" --filter-name MapQualityRankSum \
+	-filter "SOR > 10.0" --filter-name StrandOddsRatio \
+	-filter "FS > 200.0" --filter-name FisherStrandBias \
+	-filter "InbreedingCoeff < -0.8" --filter-name InbreedCoef \
+	-filter "ReadPosRankSum < -20.0" --filter-name ReadPosRank \
+	--create-output-variant-index false
+    bgzip $FILTEREDINDEL
+    tabix $FILTEREDINDEL.gz
+fi
 
- fi
- if [ ! -f $FILTEREDINDEL ]; then
-  gatk VariantFiltration --output $FILTEREDINDEL \
-  --variant $ININDEL -R $REFGENOME \
-  --cluster-window-size 10  -filter "QD < 2.0" --filter-name QualByDepth \
-  -filter "MQRankSum < -12.5" --filter-name MapQualityRankSum \
-  -filter "SOR > 4.0" --filter-name StrandOddsRatio \
-  -filter "FS > 200.0" --filter-name FisherStrandBias \
-  -filter "InbreedingCoeff < -0.8" --filter-name InbreedCoef \
-  -filter "ReadPosRankSum < -20.0" --filter-name ReadPosRank 
- fi
+if [[ ! -f $FILTEREDFIXEDINDEL.gz || $INDEL.gz -nt $FILTEREDFIXEDINDEL.gz ]]; then
+    gatk VariantFiltration --output $FILTEREDFIXEDINDEL \
+	--variant $ININDEL.gz -R $REFGENOME \
+	--cluster-window-size 10  -filter "QD < 2.0" --filter-name QualByDepth \
+	-filter "MQRankSum < -12.5" --filter-name MapQualityRankSum \
+	-filter "SOR > 10.0" --filter-name StrandOddsRatio \
+	-filter "FS > 200.0" --filter-name FisherStrandBias \
+	-filter "InbreedingCoeff < -0.8" --filter-name InbreedCoef \
+	-filter "ReadPosRankSum < -20.0" --filter-name ReadPosRank \
+	--filter-expression "AF > 0.99" --filter-name FixedAllele \
+	--create-output-variant-index false
+    bgzip $FILTEREDFIXEDINDEL
+    tabix $FILTEREDFIXEDINDEL.gz
+fi
 
- if [ ! -f $FILTEREDFIXEDINDEL ]; then
-  gatk VariantFiltration --output $FILTEREDFIXEDINDEL \
-  --variant $ININDEL -R $REFGENOME \
- #  --filter-expression "AF > 0.99" --filter-name FixedAllele \
-  --cluster-window-size 10  -filter "QD < 2.0" --filter-name QualByDepth \
-  -filter "MQRankSum < -12.5" --filter-name MapQualityRankSum \
-  -filter "SOR > 4.0" --filter-name StrandOddsRatio \
-  -filter "FS > 200.0" --filter-name FisherStrandBias \
-  -filter "InbreedingCoeff < -0.8" --filter-name InbreedCoef \
-  -filter "ReadPosRankSum < -20.0" --filter-name ReadPosRank 
- fi
+if [[ ! -f $SNPONLY.gz || $FILTEREDSNP -nt $SNPONLY.gz ]]; then
+    gatk SelectVariants -R $REFGENOME \
+	--variant $FILTEREDSNP \
+	--output $SNPONLY \
+	--exclude-filtered --create-output-variant-index false
+    bgzip $SNPONLY
+    tabix $SNPONLY.gz
+fi
 
- if [ ! -f $SNPONLY ]; then
-   gatk SelectVariants -R $REFGENOME \
-   --variant $FILTEREDSNP \
-   --output $SNPONLY \
-   --exclude-filtered
- fi
+if [[ ! -f $INDELONLY.gz || $FILTEREDINDEL -nt $INDELONLY.gz ]]; then
+    gatk SelectVariants -R $REFGENOME \
+	--variant $FILTEREDINDEL \
+	--output $INDELONLY \
+	--exclude-filtered --create-output-variant-index false
+    bgzip $INDELONLY
+    tabix $INDELONLY.gz
+fi
 
- if [ ! -f $INDELONLY ]; then
-   gatk SelectVariants -R $REFGENOME \
-   --variant $FILTEREDINDEL \
-   --output $INDELONLY \
-   --exclude-filtered 
- fi
+if [[ ! -f $SNPNOFIXED.gz || $FILTEREDFIXEDSNP -nt $SNPNOFIXED.gz ]]; then
+    gatk SelectVariants -R $REFGENOME \
+	--variant $FILTEREDFIXEDSNP \
+	--output $SNPNOFIXED \
+	--exclude-filtered --create-output-variant-index false
+    bgzip $SNPNOFIXED
+    tabix $SNPNOFIXED.gz
+fi
 
- if [ ! -f $SNPNOFIXED ]; then
-     gatk SelectVariants -R $REFGENOME \
-	 --variant $FILTEREDFIXEDSNP \
-	 --output $SNPNOFIXED \
-	 --exclude-filtered
- fi
-
- if [ ! -f $INDELNOFIXED ]; then
-     gatk SelectVariants -R $REFGENOME \
-	 --variant $FILTEREDFIXEDINDEL \
-	 --output $INDELNOFIXED \
-	 --exclude-filtered
-
- fi
+if [[ ! -f $INDELNOFIXED.gz || $FILTEREDFIXEDINDEL -nt $INDELNOFIXED.gz ]]; then
+    gatk SelectVariants -R $REFGENOME \
+	--variant $FILTEREDFIXEDINDEL \
+	--output $INDELNOFIXED \
+	--exclude-filtered --create-output-variant-index false
+    bgzip $INDELNOFIXED
+    tabix $INDELNOFIXED.gz
+fi
